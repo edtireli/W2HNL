639c0d258b75b0dbc321a39cb0e9379a60cdddbb
Edis Tirei <edtireli@gmail.com>
2024-02-01 21:54:28 +0100

    initial commits

--- parameters/data_parameters.py
+++ parameters/data_parameters.py
@@ -1,2 +1,2 @@
-
+# A parameter file for various dimensions and variables to do with the raw data previously generated
 batch_size = 9999 # Number of events generated per run (mass) in MadGraph
--- parameters/experimental_parameters.py
+++ parameters/experimental_parameters.py
@@ -1,3 +1,7 @@
+# A parameter file for various dimensions and variables to do with the experimental constraints, such as cut conditions, efficiencies etc.
 
-DV_min = '120 mm'
-DV_max = '300 mm'
+pseudorapidity_minimum = 0   # Minimum pseudorapidity criteria for HNL decay 
+pseudorapidity_maximum = 2.4 # Maximum pseudorapidity criteria for HNL decay
+
+r_min = '120 mm' # Minimum distance from interaction point (IP) to consider HNL decays/decay volume
+r_max = '300 mm' # Maximum distance from interaction point (IP) to consider HNL decays/decay volume

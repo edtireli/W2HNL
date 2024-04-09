ce0d003e97905d0ea5ff53ca189ad004ebd74c28
Edis Tirei <edtireli@gmail.com>
2024-04-09 15:03:11 +0200

    bugfixes

--- parameters/data_parameters.py
+++ parameters/data_parameters.py
@@ -6,7 +6,7 @@ prompt_lepton_flavour = 3 # 1 = electron, 2 = muon, 3 = tau
 data_type = 'LHE' # Either "HEPMC" or "LHE" format
 
 #mass_hnl = [i*0.5 for i in range(2,21)] # HNL masses from data in GeV
-mass_hnl = [i*0.25 for i in range(1,41)] # HNL masses from data in GeV
+mass_hnl = [i*0.25 for i in range(2,41)] # HNL masses from data in GeV
 
 flavour_hnl = 3 # 1=e, 2=mu, 3=tau
 batch_size = 25000 # Number of events generated per run (mass) in MadGraph

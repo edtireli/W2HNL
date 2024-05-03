9c29f51173585b7c530bad60b07a23a2d08da2ea
Edis Tirei <edtireli@gmail.com>
2024-05-03 15:33:37 +0200

    Bugfixes to ROOT loading

--- utils/ROOT_processing.py
+++ utils/ROOT_processing.py
@@ -76,8 +76,8 @@ def root_data_processing(base_folder):
         
         print("Processing complete.")
         print({key: np.shape(data_structure[key]) for key in data_structure})  # Print shapes to verify structure
-        return (np.array(data_structure['W_boson']), np.array(data_structure['HNL']), np.array(data_structure['prompt_lepton']),
-                np.array(data_structure['dilepton_minus']), np.array(data_structure['dilepton_plus']), np.array(data_structure['neutrino']))
+        return (data_structure['W_boson'], data_structure['HNL'], data_structure['prompt_lepton'],
+                data_structure['dilepton_minus'], data_structure['dilepton_plus'], data_structure['neutrino'])
     
     except Exception as e:
         print(f"An error occurred: {e}")

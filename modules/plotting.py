9bd49304aba1f62a0925bfa2fd60513b620ee4da
Edis Tirei <edtireli@gmail.com>
2024-09-24 16:10:40 +0200

    bugfix

--- modules/plotting.py
+++ modules/plotting.py
@@ -1293,14 +1293,15 @@ def plotting(momenta, batch, production_arrays, arrays):
     plot_parameter_space_region(production_allcuts, title='HNL Production (all cuts)', savename = f'hnl_production_allcuts_{luminosity}_{invmass_cut_type}')    
     plot_parameter_space_regions(production_nocuts, production_pT, production__pT_rap, production__pT_rap_invmass, production_allcuts, labels=['no cuts', '$p_T$-cut', '($p_T \\cdot \\eta$)-cut', '($p_T \\cdot \\eta \\cdot m_0$)-cut', '($p_T \\cdot \\eta \\cdot m_0 \\cdot \Delta_R \\cdot DV$)-cut'], colors=['red', 'blue', 'green', 'purple', 'black'], smooth=False, sigma=1, savename='hnl_production_parameter_space_multi') 
     
-    plot_invariant_mass_cut_histogram(
-        momenta, 
-        survival_invmass_displaced, 
-        r_lab, 
-        mass_hnl, 
-        mixing, 
-        production_allcuts,
-    )
+    if invmass_cut_type == 'nontrivial':
+        plot_invariant_mass_cut_histogram(
+            momenta, 
+            survival_invmass_displaced, 
+            r_lab, 
+            mass_hnl, 
+            mixing, 
+            production_allcuts,
+        )
     
     if production_plots:
         plot_parameter_space_region(production_invmass, title='HNL Production (invariant mass cut)', savename = 'hnl_production_invmass')
